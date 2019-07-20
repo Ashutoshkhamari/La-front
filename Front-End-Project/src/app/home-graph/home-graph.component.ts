21f2097ad658f7ae7303f4025453cd2a34eb4bfa
Ashutosh <ashutoshkhamari.gdn@gmail.com>
2019-07-20 12:38:07 +0530

    new change

--- Front-End-Project/src/app/home-graph/home-graph.component.ts
+++ Front-End-Project/src/app/home-graph/home-graph.component.ts
@@ -9,8 +9,8 @@ export class HomeGraphComponent implements OnInit {
   ngOnInit() {
     var chart = new CanvasJS.Chart("chartContainer", {
       animationEnabled: true,
-      theme: "light1", // "light1", "light2", "dark1", "dark2"
-      backgroundColor: "rgba(225,150,150,0.5)",
+      theme: "dark", // "light1", "light2", "dark1", "dark2"
+      backgroundColor: "rgba(225,150,150,1)",
       title:{
         text: "Donation Analysis"
       },
